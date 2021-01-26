46d60d66aba129f59c6258b1d8c508df8120e374
abdykapar <abdykaparkamilov@gmail.com>
2021-01-26 18:06:02 +0600

    new FDM template

---
+++ src/components/Loader.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		<!-- BEGIN LOADER -->
+		<div id="load_screen">
+			<div class="loader">
+				<div class="loader-content">
+					<div class="spinner-grow align-self-center"></div>
+				</div>
+			</div>
+		</div>
+		<!--  END LOADER -->
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'Loader',
+	};
+</script>
+
+<style></style>
---
+++ src/store/account.module.js
@@ -0,0 +1,38 @@
+import {userService} from '@/_services/user.service'
+import router from '@/router'
+
+const user = JSON.parse(localStorage.getItem('user'))
+
+const state = {
+	user: user ? user : {},
+}
+
+const actions = {
+	login({commit, dispatch}, data) {
+		userService.login(data).then(res => {
+			localStorage.setItem('user', JSON.stringify(res));
+			commit('SET_USER', res);
+			router.push('/');
+		}).catch(err => console.log(err));
+	},
+	logout({commit}) {
+		userService.logout();
+		commit('REMOVE_USER');
+	}
+}
+
+const mutations = {
+	SET_USER(state, data) {
+		state.user = data;
+	},
+	REMOVE_USER(state) {
+		state.user = null;
+	},
+}
+
+export const account = {
+	namespaced: true,
+	state,
+	actions,
+	mutations
+}
