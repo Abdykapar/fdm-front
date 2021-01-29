54772678edd1bf057ffd3a59b91dabebad763f42
abdykapar <abdykaparkamilov@gmail.com>
2021-01-29 15:14:41 +0600

    add header and sidebar components

---
+++ src/components/Header.vue
@@ -0,0 +1,96 @@
+<template>
+	<div class="header">
+		<div class="header__left"></div>
+		<div class="header__center">
+			<img src="../assets/icons/search.svg" alt="" />
+			<input type="text" placeholder="Search here" />
+		</div>
+		<div class="header__right">
+			<div class="header__right__alert">
+				<img src="../assets/icons/alert.svg" alt="" />
+				<svg
+					class="alert"
+					width="6"
+					height="6"
+					viewBox="0 0 6 6"
+					fill="none"
+					xmlns="http://www.w3.org/2000/svg"
+				>
+					<circle cx="3" cy="3" r="3" fill="#FA5F1C" />
+				</svg>
+			</div>
+			<div class="header__right__profile flex-align-center">
+				<img src="../assets/img/img.png" alt="" />
+				<span class="header__right__name">Begimay Maslinova</span>
+				<img src="../assets/icons/chevron-down.svg" alt="" />
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'Header',
+	};
+</script>
+
+<style lang="scss">
+	.header {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		height: 100px;
+
+		&__center {
+			position: relative;
+			input {
+				border: 0.5px solid #888e9a;
+				width: 400px;
+				height: 39px;
+				background: $black-back;
+				padding-left: 42px;
+				font-weight: 400;
+				font-size: 14px;
+				line-height: 19px;
+				color: #7882a2;
+				outline-color: #888e9a;
+			}
+
+			img {
+				position: absolute;
+				left: 16px;
+				top: 9px;
+			}
+		}
+
+		&__right {
+			display: flex;
+			align-items: center;
+
+			&__alert {
+				margin-right: 42px;
+				cursor: pointer;
+				position: relative;
+
+				.alert {
+					position: absolute;
+					top: -2px;
+					right: 3px;
+				}
+			}
+
+			&__profile {
+				font-weight: 400;
+				font-size: 16px;
+				line-height: 22px;
+				letter-spacing: 0.02em;
+				color: #ffffff;
+				margin-right: 40px;
+			}
+
+			&__name {
+				padding: 0 13px 0 16px;
+			}
+		}
+	}
+</style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,97 @@
+<template>
+	<div class="sidebar">
+		<div class="sidebar__top">
+			<div class="sidebar__logo">
+				<span>FlightData</span>
+				<span class="sidebar__circle"></span>
+			</div>
+			<div class="sidebar__menu">
+				<div class="sidebar__menu__item">
+					<img src="../assets/icons/bar-chart.svg" alt="" /> Dashboard
+				</div>
+				<div class="sidebar__menu__item">
+					<img src="../assets/icons/folder.svg" alt="" /> Data List
+				</div>
+				<div class="sidebar__menu__item">
+					<img src="../assets/icons/monitor.svg" alt="" /> Data Insights
+				</div>
+				<div class="sidebar__menu__item active">
+					<img src="../assets/icons/user.svg" alt="" /> Technician Users
+				</div>
+			</div>
+		</div>
+		<div class="sidebar__bottom">
+			<div class="sidebar__menu__item">
+				<img src="../assets/icons/settings.svg" alt="" /> Settings
+			</div>
+			<div class="sidebar__menu__item">
+				<img src="../assets/icons/help.svg" alt="" /> Help
+			</div>
+			<div class="sidebar__menu__item">
+				<img src="../assets/icons/logout.svg" alt="" /> Logout
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'Sidebar',
+	};
+</script>
+
+<style lang="scss">
+	.sidebar {
+		width: 240px;
+		background: #22262a;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		min-height: 100vh;
+
+		&__logo {
+			font-weight: 400;
+			font-size: 24px;
+			line-height: 28px;
+			color: #ffffff;
+			padding-top: 23px;
+			padding-bottom: 56px;
+			text-align: center;
+		}
+
+		&__circle {
+			background: linear-gradient(225deg, #fa2b56 0%, #f91c3d 100%);
+			width: 10px;
+			height: 10px;
+			display: inline-block;
+			border-radius: 50%;
+			margin-left: 4px;
+		}
+
+		&__menu {
+			&__item {
+				display: flex;
+				align-items: center;
+				font-weight: 400;
+				font-size: 15px;
+				line-height: 20px;
+				letter-spacing: 0.02em;
+				color: #ffffff;
+				padding: 15px 0 15px 43px;
+				cursor: pointer;
+
+				&:hover {
+					background: rgba($color: #ffffff, $alpha: 0.05);
+				}
+
+				img {
+					padding-right: 20px;
+				}
+
+				&.active {
+					background: rgba($color: #ffffff, $alpha: 0.1);
+				}
+			}
+		}
+	}
+</style>
