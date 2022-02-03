6eac8faf6d9addec5b03c06101c82c798e22fe7b
Abdykapar Kamilov <abdykaparkamilov@mail.ru>
2022-02-03 13:08:40 +0600

    test entity

--- src/_helpers/authHeader.js
+++ src/_helpers/authHeader.js
@@ -14,11 +14,12 @@ export function authHeader () {
 export function handleResponse (response) {
 	return response.text().then(text => {
 		const data = text && JSON.parse(text)
+		console.log(response)
 		if (!response.ok) {
 			if (response.status === 401 || response.status === 403) {
-				userService.logout()
-				window.location.href = '/login'
-				location.reload(true)
+				// userService.logout()
+				// window.location.href = '/login'
+				// location.reload(true)
 			}
 
 			const error = (data && data.message) || response.statusText
